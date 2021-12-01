4219ac212f087730d9c89365ffbb2d277316e579
Romain <axilais.romain@gmail.com>
2021-12-01 19:37:05 +0100

    Modif API-gateway

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const request = require('request');
 const axios = require('axios');
 
 const port = process.argv.slice(2)[0];
@@ -12,8 +11,11 @@ app.use(bodyParser.json());
 const heroesService = 'http://localhost:8081';
 const threatsService = 'http://localhost:8082';
 
+const gameService = 'http://localhost:8083';
+const threatsService = 'http://localhost:8084';
+
 // Appeler et afficher les données du micro-service THREATS
-app.get('/api/threats', (req, res) => {
+app.get('/gateway/threats', (req, res) => {
     axios.get(`${threatsService}/threats`)
       .then(function (res) {
         console.log(res.data);
@@ -27,38 +29,23 @@ app.get('/api/threats', (req, res) => {
     console.log('API threats appelée')
 });
 
-// Appeler et afficher les données du micro-service HEROES
-app.get('/api/heroes', (req, res) => {
-    axios.get(`${heroesService}/heroes`)
-      .then(function (res) {
-        console.log(res.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      }); 
-    console.log('API heroes appelée')
-});
-
-// Appeler et afficher les données du micro-service HEROES
-app.get('/api/heroes', (req, res) => {
-    axios.get(`${heroesService}/heroes`)
-      .then(function (res) {
-        console.log(res.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      }); 
-    console.log('API heroes appelée')
+// Appeler et afficher les catégories du micro-service GAME
+app.get('/gateway/game/categories', (req, res) => {
+  axios.get(`${gameService}/categories`)
+    .then(function (res) {
+      console.log('API Game appelée -> Affichage des catégories')
+      console.log(res.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    }); 
 });
 
 // Ajouter un utilisateur -> n'est relié à rien mais c'est juste pour te montrer la forme pour envoyer des données
-app.post('/api/register', (req, res) => {
+app.post('/gateway/register', (req, res) => {
     axios.post(`${heroesService}/user/add`, {
         email: 'test@mail.com',
         password: 'mdfkoejo'
